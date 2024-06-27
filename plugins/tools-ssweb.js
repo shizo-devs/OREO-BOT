91614fe527a259d663f354a30b111bb4862f89b7
Shizo Devs <codeX.shizothetechie@gmail.com>
2024-06-27 14:40:53 +0530

    Delete plugins directory

--- plugins/tools-ssweb.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn, command, args }) => {
-if (!args[0]) return conn.reply(m.chat, '*Give me the link dumbo 😂*', m)
-let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
-conn.sendFile(m.chat, ss, 'xei.png', args[0], m)}
-handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
-handler.tags = ['tools']
-handler.command = /^ss(web)?f?$/i
-export default handler
